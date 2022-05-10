b36a6279ce48bd740e8f7a6c6f3944803174ccc6
Shruti Patel <76270372+shrutijpatel@users.noreply.github.com>
2022-05-10 23:49:39 +0530

    Update twoSum.java

--- Array/Easy/twoSum.java
+++ Array/Easy/twoSum.java
@@ -1,21 +1,9 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int res[]=new int[2]; //returning array
-        int flag=0;
-        for(int i=0;i<nums.length;i++){
-            int j=i+1;
-            while(j<nums.length){
-                if(nums[i]+nums[j]==target){ //if elements found
-                    res[0]=i;
-                    res[1]=j;
-                    flag++;
-                    break;
-                }
-                else j++;
-            }
-            if(flag==1)
-                break;
-        }
-        return res;
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for (int i = 0; i < nums.length; map.put(nums[i], i++))
+            if (map.containsKey(target - nums[i]))
+                return new int[] {map.get(target - nums[i]), i};
+        return new int[] {0, 0};
     }
 }
